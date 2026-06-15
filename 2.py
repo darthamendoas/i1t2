9bfd9ca45d45fe32ac57c8c8fec4d7190a0d2413
Miguel Justus Amendoeira <miguel.amendoeira@ort.org.br>
2026-06-15 14:29:43 -0300

    Remove 'Lucas' from alunos and append to turma

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@ turma = [('Ana', 20), ('Bruno', 22)]
 
 print(('Bruno',22) in turma)
 
-alunos=[('Lucas',21),('Marcelo',67),('João',42)]
+alunos=[('Marcelo',67),('João',42)]
 turma.append(alunos)
 
 print(len(turma))
