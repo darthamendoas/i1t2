4801ef33a1afd08ece38bb37238793bea172c1e7
darthamendoas <miguel.amendoeira@ort.org.br>
2026-06-15 14:25:04 -0300

    dadasda

---
+++ 1.py
@@ -0,0 +1,8 @@
+turma = [('Ana', 20), ('Bruno', 22)]
+turma.append(('Carlos',19))
+
+print(turma[0])
+
+turma.remove(turma[0])
+
+#Eu removeria com o .remove a primeira tupla e criaria outra na mesma posição(0)
---
+++ 2.py
@@ -0,0 +1,10 @@
+turma = [('Ana', 20), ('Bruno', 22)]
+
+print(('Bruno',22) in turma)
+
+alunos=[('Lucas',21),('Marcelo',67),('João',42)]
+turma.append(alunos)
+
+print(len(turma))
+
+#Não funciona,pois a tupla é imutavel,mesmo quando dentro de uma lista
